f024f9a6d841660f11d21088cbc999470a5f7fff
Stunner05 <aipresh05@gmail.com>
2022-06-30 18:39:07 -0700

    g

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
+ * main - Entry point
  *
- *  * main - Entry point
+ * 
  *
- *   * Return:1
+ * Return: Always 1 (Success)
  *
- *    */
+ */
 
 int main(void)
+
 {
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, ch, sizeof(ch));
+fprintf(stderr, "and that piece of art is useful\" - \ Dora Korpar, 2015-10-19\n");
+
 return (1);
+
 }
-
-
