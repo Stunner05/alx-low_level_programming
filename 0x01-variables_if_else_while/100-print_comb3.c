c7008d450aeea44113bdc0310d6ebd6aaebde748
Stunner05 <aipresh05@gmail.com>
2022-07-01 18:33:38 -0700

    add

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -23,52 +23,34 @@ int main(void)
 
 	while (d < 10)
 	{
-	       c = 0;
+		c = 0;
 		
-	       while (c < 10)
+		while (c < 10)
 	       {
 		       if (d != c && d < c)
 		       {
 			       putchar('0' + d);
-
+			       
 			       putchar('0' + c);
-
+			       
 			       if (c + d != 17)
 			       {
 				       putchar(',');
 
 				       putchar(' ');
+			       
 			       }
-		       }
-		       
-		       c++;
 
+		       }
+
+		       c++
 	       }
 	       
 	       d++;
 
 	}
-
 	putchar('\n');
 
 	return (0);
 
 }
-
-
-
-		
-
-
-
-
-
-
-
-
-
-
-
-
-
-
