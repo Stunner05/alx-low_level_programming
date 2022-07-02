4f75530355a1a89806f54d897c937a3fccb8b351
Stunner05 <aipresh05@gmail.com>
2022-07-01 18:42:11 -0700

    t

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -24,13 +24,11 @@ int main(void)
 	while (d < 10)
 	{
 		c = 0;
-		
 		while (c < 10)
 	       {
 		       if (d != c && d < c)
 		       {
 			       putchar('0' + d);
-			       
 			       putchar('0' + c);
 			       
 			       if (c + d != 17)
