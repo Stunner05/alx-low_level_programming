dda7bc96a5413b0f6c8747d6eddcadbe43ce0873
Stunner05 <aipresh05@gmail.com>
2022-07-05 09:55:57 -0700

    new

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,19 @@
-void print_alphabet_x10(void);
+#include "main.h"
+/**
+ * main - main block
+ * Description: a function that prints 10 times the alphabet.
+ * Return 0 success
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	char c;
+
+	for ( i = 0; i < 10: i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		-putchar('\n');
+	}
+}
