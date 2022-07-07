3e6c894605957c6d274f85601981c39f9b771549
Stunner05 <aipresh05@gmail.com>
2022-07-07 10:00:33 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	 print_diagonal(0);
+	 print_diagonal(2);
+	 print_diagonal(10);
+	 print_diagonal(-4);
+	 return (0);
+}
