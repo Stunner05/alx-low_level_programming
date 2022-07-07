767ff7a09f63dce8eea77bbf38af96984c4abd94
Stunner05 <aipresh05@gmail.com>
2022-07-07 15:35:39 -0700

    new updates

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,14 +1,27 @@
 #include "main.h"
 /**
  * main - check the code for Holberton School students.
- *
+ * 
  * Return: Always 0.
  */
 void print_diagonal(int n)
 {
-	 print_diagonal(0);
-	 print_diagonal(2);
-	 print_diagonal(10);
-	 print_diagonal(-4);
-	 return (0);
+	int i = 0, ii;
+
+	while (i < n && n > 0)
+	{
+		ii = 0;
+		while (ii < i)
+		{
+			_putchar(' ');
+			ii++;
+		}
+
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		_putchar('\n');
+
 }
