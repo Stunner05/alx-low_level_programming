bd59b2b94af8ae5339da287dba53c0cbb6ef24c5
Stunner05 <aipresh05@gmail.com>
2022-07-07 20:15:16 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,5 +4,21 @@
  * Description: a function that prints 10 times the numbers
  * Return: 0 success
  */
+void more_numbers(void)
+{
+	int x,y;
 
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 15; y++)
+		{
+			if (y >= 10)
+			{
+				_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
 
