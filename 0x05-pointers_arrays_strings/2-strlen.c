d869f1a7bbef5076ede16ef7128b9f4d9d5650fa
Stunner05 <aipresh05@gmail.com>
2022-07-12 16:10:41 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - calculates string length
+ * @s: passes argument
+ *
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (s[a])
+	{
+		i++
+	}
+	return (i);
+}
