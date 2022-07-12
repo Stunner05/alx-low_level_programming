e1df5017be73c8385b4c82cd2d551899c86fc58f
Stunner05 <aipresh05@gmail.com>
2022-07-12 04:49:10 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that  updates the value it points to
+ * @n:parameter to be used
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
