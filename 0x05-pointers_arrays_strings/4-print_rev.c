758ef32586ab22d1d32b61aabb74b7e14ca602e7
Stunner05 <aipresh05@gmail.com>
2022-07-12 17:32:47 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - a function that reverses a string.
+ * @s: passes an argument
+ *
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	i = 0;
+
+	while (s[i])
+	{ i++
+	}
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n')
