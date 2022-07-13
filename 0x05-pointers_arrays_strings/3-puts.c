a52f82f2214163d6488837589c68565ae281ddd7
Stunner05 <aipresh05@gmail.com>
2022-07-12 17:06:03 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - a function that prints a string
+ * @str: passes an argument
+ *
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a])
+	{
+		_putchar(str[i])
+			a++;
+	}
+	_putchar('\n')
+}
