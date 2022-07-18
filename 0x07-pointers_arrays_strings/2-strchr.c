cf24989a2f6df2ff074465b140e6be16e98798e0
Stunner05 <aipresh05@gmail.com>
2022-07-18 14:13:16 -0700

    new

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,20 @@
 #include "main.h"
 /**
- * _strchr -
- * @s:
- * @c:
- * Return:
+ * _strchr - a function that locates a character in a string.
+ * @s: paramter to be returned
+ * @c: parameter to be checked
+ * Return: s
  */
 char *_strchr(char *s, char c)
 {
+	int i;
+
+	for (i = 0; i >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
