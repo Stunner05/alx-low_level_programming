b36d2d5d27b9599151c2b3c9009d603fcbb12bbc
Stunner05 <aipresh05@gmail.com>
2022-07-21 17:51:59 -0700

    update

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,12 +1,52 @@
 #include "main.h"
 
+int check(char *s, int start, int end, int pair);
+int last_index(char *s);
+
 /**
- * is_palindrome - a function that returns 1 if a string is a palindrome and 0 if not.
+ * is_palindrome - a function that checks for palindrome
  * @s: string parameter
  * Return: 1 or 0
  *
- * */
+ */
 
 int is_palindrome(char *s)
 {
+	int end = last_index(s);
 
+	return (check(s, 0, end - 1, end % 2));
+}
+/**
+ * last_index - checks the character.
+ * @s: parameter
+ * Return: success
+ */
+int last_index(char *s)
+{
+	int n = 0;
+
+	if (*s > '\0')
+	{
+		n += last_index(s + 1) + 1;
+	}
+	return (n);
+}
+
+/**
+ * check - checker for the palindrome
+ * @s: string
+ * @start: int moves from right to left
+ * @end: int moves from left to right
+ * @pair: int
+ * Return: 0 or 1
+ */
+
+int check(char *s, int start, int end, int pair)
+{
+	if ((start == end && pair != 0) || (start == end + 1 && pair == 0))
+		return (1);
+	else if (s[start] != s[end])
+		return (0);
+	else
+		return (check(s, start + 1, end - 1, pair));
+}
