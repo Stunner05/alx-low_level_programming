c137ad0cfd4e6c7fc80823126f147936e7951f63
Stunne05 <aipresh05@gmail.com>
2022-07-26 16:59:09 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - a program that prints all arguments it receives.
+ * @argc: arguments number
+ * @argv: array of arguments
+ * Return: success
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
+}
