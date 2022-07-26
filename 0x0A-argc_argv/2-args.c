4fddde241ca168d2f905585b69edaced502943d4
Stunne05 <aipresh05@gmail.com>
2022-07-26 05:31:36 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - a program that prints all arguments it receives.
+ * @argc: arguments number
+ * @argv: array of arguments
+ * Return: success
+ */
+int main(int argcc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", i, argv[i])
+	}
+	return (0);
+}
