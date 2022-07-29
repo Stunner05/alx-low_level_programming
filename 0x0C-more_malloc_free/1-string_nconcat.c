203cf57fe1ca4d887ff64ca27c94314938f19a6f
Stunne05 <aipresh05@gmail.com>
2022-07-29 05:57:17 -0700

    taask 0

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - a function that allocates memory using malloc.
+ * @b: parameter to be assigned
+ *
+ * Return:
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
