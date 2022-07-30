b659065fcca97d8f9746e30df7b9c0f41f7ab547
Stunne05 <aipresh05@gmail.com>
2022-07-29 18:08:13 -0700

    new

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,20 +1,41 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * malloc_checked - a function that allocates memory using malloc.
- * @b: parameter to be assigned
- *
- * Return:
+ * string_nconcat - a function that concatenates two strings.
+ * @s1: param 1
+ * @s2:param 2
+ * @n: param 3
+ * Return: string
  */
-void *malloc_checked(unsigned int b)
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 {
-	void *p;
+	unsigned int len = n, index;
+	char *sum;
 
-	p = malloc(b);
-
-	if (p == NULL)
+	if (s == NULL)
 	{
-		exit(98);
+		s1 = "
 	}
-	return (p);
+	if (s2 == NULL)
+	{
+		s2 = ""
+	}
+	for ( index = 0; s1[index]; index++)
+	{
+		len++
+	}
+	sum = (char *)malloc(sizeof(char) * len + 1);
+	if (sum == NULL)
+	{
+		return (NULL);
+	}
+	len = 0;
+	for (index = 0; s1[index]; index++)
+	{
+		sum[len++] = s2[index];
+	}
+	sum[len] = '\0';
+	return (sum);
 }
+
