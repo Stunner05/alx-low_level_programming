47b80a230902a6433b12b504453bb6ac81ec46bb
Stunne05 <aipresh05@gmail.com>
2022-07-31 20:32:21 -0700

    new

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,6 +34,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len = 0;
 	for (index = 0; s1[index]; index++)
 	{
+		sum[len++] = s1[index];
+	}
+	for (index = 0; s2[index] && index < n; index++)
+	{
 		sum[len++] = s2[index];
 	}
 	sum[len] = '\0';
