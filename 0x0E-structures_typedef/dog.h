9171601a5986db7d0d9c6f74f329fd1e53df9c46
Stunne05 <aipresh05@gmail.com>
2022-08-01 18:04:29 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure definition of a dog
+ * @name: character parameter
+ * @age: floa parameter
+ * @owner: character parameter 2
+ */
+typedef struct dog {
+	char *name;
+	float *age;
+	char *owner;
+} dog;
+
+#endif
